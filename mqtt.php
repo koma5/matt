2e75a0f6364a41cd599cfe316fb98f6dae26b47d
Marco Koch <marco@5th.ch>
2014-11-02 21:27:31 +0100

    long ago: fixed javascript object oriented device structure added second device

--- mqtt.php
+++ mqtt.php
@@ -9,7 +9,7 @@ if (isset($_REQUEST['t']))
 
 	echo "topic:" . $topic;
 	echo "</br>payload: " . $payload;
-	$mqtt = new phpMQTT("127.0.0.1", 1883, "PHP MQTT Bridge");
+	$mqtt = new phpMQTT("172.16.0.70", 1883, "PHP MQTT Bridge");
 
 	if ($mqtt->connect())
 	{
